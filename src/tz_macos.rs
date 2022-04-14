67d12303a17a47039b1fbd474653decbfd0cef99
René Kijewski <rene.kijewski@fu-berlin.de>
2022-04-14 23:43:24 +0200

    Remove need for reallocations for MacOS implementation

--- src/tz_macos.rs
+++ src/tz_macos.rs
@@ -4,13 +4,17 @@ pub(crate) fn get_timezone_inner() -> Result<String, crate::GetTimezoneError> {
     // Get string like ""Europe/Berlin (GMT+2) offset 7200 (Daylight)""
     let mut str1 = format!("{:?}", tz);
 
-    // strip leading double quotes
-    while str1.starts_with('"') {
-        str1 = str1[1..].to_string();
+    // We split the string first, so removing the leading quotes has to move fewer characters.
+    str1.truncate(match str1.split_ascii_whitespace().next() {
+        Some(s) => s.len(),
+        None => return Err(crate::GetTimezoneError::FailedParsingString),
+    });
+
+    let trim_amount = str1.len() - str1.trim_start_matches('"').len();
+    if trim_amount == 0 {
+        return Err(crate::GetTimezoneError::FailedParsingString);
     }
 
-    match str1.split_whitespace().next() {
-        Some(s) => Ok(s.to_string()),
-        None => Err(crate::GetTimezoneError::FailedParsingString),
-    }
+    str1.replace_range(..trim_amount, "");
+    Ok(str1)
 }
