a8ee0136ede04d531d79fcb25a0778017d4a72ac
Andrew Straw <strawman@astraw.com>
2022-04-15 17:31:54 +0200

    do not fake chrono-tz crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,18 @@
 //! [`get_timezone()`](fn.get_timezone.html) function.
 //!
 //! ```rust
-//! # mod chrono_tz { pub type Tz = String; }
 //! // Get the current time zone as a string.
-//! let tz_str = iana_time_zone::get_timezone().unwrap();
+//! let tz_str = iana_time_zone::get_timezone()?;
 //! println!("The current time zone is: {}", tz_str);
+//! # Ok::<(), iana_time_zone::GetTimezoneError>(())
+//! ```
 //!
-//! // Convert the time zone string to a `chrono-tz::Tz` variant.
-//! let tz: chrono_tz::Tz = tz_str.parse().unwrap();
-//! println!("The current time zone is: {}", tz);
+//! The resulting string can be parsed to a
+//! [`chrono-tz::Tz`](https://docs.rs/chrono-tz/latest/chrono_tz/enum.Tz.html)
+//! variant like this:
+//! ```ignore
+//! let tz_str = iana_time_zone::get_timezone()?;
+//! let tz: chrono_tz::Tz = tz_str.parse()?;
 //! ```
 
 #[cfg(target_os = "linux")]
