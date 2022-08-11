bf6177d43713ca5dc905888a600ede07047b0563
René Kijewski <rene.kijewski@fu-berlin.de>
2022-08-11 12:36:41 +0200

    Use core-foundation-sys instead of core-foundation
    iana-time-zone is used by [chrono], and chrono is (optionally) used by
    [core-foundation]. So if iana-time-zone uses core-foundation, this
    results in a circular dependency.
    
    This PR replaces core-foundation by [core-foundation-sys], which breaks
    the circular dependency.
    
    [chrono]: https://crates.io/crates/chrono
    [core-foundation]: https://crates.io/crates/core-foundation
    [core-foundation-sys]: https://crates.io/crates/core-foundation-sys

--- src/tz_macos.rs
+++ src/tz_macos.rs
@@ -1,5 +1,41 @@
+use core_foundation_sys::base::{CFRelease, CFTypeRef};
+use core_foundation_sys::string::{kCFStringEncodingUTF8, CFStringGetCStringPtr};
+use core_foundation_sys::timezone::{CFTimeZoneCopySystem, CFTimeZoneGetName};
+
 pub(crate) fn get_timezone_inner() -> Result<String, crate::GetTimezoneError> {
-    Ok(core_foundation::timezone::CFTimeZone::system()
-        .name()
-        .to_string())
+    unsafe {
+        Dropping::new(CFTimeZoneCopySystem())
+            .and_then(|tz| Dropping::new(CFTimeZoneGetName(tz.0)))
+            .and_then(|name| {
+                let name = CFStringGetCStringPtr(name.0, kCFStringEncodingUTF8);
+                if name.is_null() {
+                    None
+                } else {
+                    Some(name)
+                }
+            })
+            .and_then(|name| std::ffi::CStr::from_ptr(name).to_str().ok())
+            .map(|name| name.to_owned())
+            .ok_or(crate::GetTimezoneError::OsError)
+    }
+}
+
+struct Dropping<T>(*const T);
+
+impl<T> Drop for Dropping<T> {
+    #[inline]
+    fn drop(&mut self) {
+        unsafe { CFRelease(self.0 as CFTypeRef) };
+    }
+}
+
+impl<T> Dropping<T> {
+    #[inline]
+    unsafe fn new(v: *const T) -> Option<Self> {
+        if v.is_null() {
+            None
+        } else {
+            Some(Self(v))
+        }
+    }
 }
