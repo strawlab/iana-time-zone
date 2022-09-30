b6d8caa4fcb463109381e5101bbbff7f9be52429
René Kijewski <rene.kijewski@fu-berlin.de>
2022-09-30 05:45:08 +0200

    Wrap C++ code in try-catch

--- src/impl_haiku.cc
+++ src/impl_haiku.cc
@@ -8,33 +8,37 @@
 #include <TimeZone.h>
 
 size_t ::tz_haiku::get_tz(uint8_t *buf, size_t buf_len) {
-    static_assert(sizeof(char) == sizeof(uint8_t), "Illegal char size");
+    try {
+        static_assert(sizeof(char) == sizeof(uint8_t), "Illegal char size");
 
-    if (!buf || !buf_len) {
+        if (!buf || !buf_len) {
+            return 0;
+        }
+
+        BLocaleRoster *locale_roster(BLocaleRoster::Default());
+        if (!locale_roster) {
+            return 0;
+        }
+
+        BTimeZone tz(NULL, NULL);
+        if (locale_roster->GetDefaultTimeZone(&tz) != B_OK) {
+            return 0;
+        }
+
+        BString bname(tz.ID());
+        int32_t length(bname.Length());
+        if (length <= 0 || size_t(length) > buf_len) {
+            return 0;
+        }
+
+        const char *sname(bname.String());
+        if (!sname) {
+            return 0;
+        }
+
+        std::memcpy(buf, sname, length);
+        return length;
+    } catch (...) {
         return 0;
     }
-
-    BLocaleRoster *locale_roster(BLocaleRoster::Default());
-    if (!locale_roster) {
-        return 0;
-    }
-
-    BTimeZone tz(NULL, NULL);
-    if (locale_roster->GetDefaultTimeZone(&tz) != B_OK) {
-        return 0;
-    }
-
-    BString bname(tz.ID());
-    int32_t length(bname.Length());
-    if (length <= 0 || size_t(length) > buf_len) {
-        return 0;
-    }
-
-    const char *sname(bname.String());
-    if (!sname) {
-        return 0;
-    }
-
-    std::memcpy(buf, sname, length);
-    return length;
 }
