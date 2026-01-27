187c2a972df1566b77840fe6d1d55de8da9872cf
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2026-01-27 14:20:46 +0100

    Drop `emscripten-functions` dependency

--- src/tz_wasm32_emscripten.rs
+++ src/tz_wasm32_emscripten.rs
@@ -1,7 +1,30 @@
 use crate::GetTimezoneError;
-use emscripten_functions::emscripten::run_script_string;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::ptr::NonNull;
+
+extern "C" {
+    fn emscripten_run_script_string(script: *const c_char) -> *mut c_char;
+}
 
 pub(crate) fn get_timezone_inner() -> Result<String, GetTimezoneError> {
-    run_script_string("Intl.DateTimeFormat().resolvedOptions().timeZone")
-        .ok_or_else(|| GetTimezoneError::OsError)
+    const SCRIPT: &CStr = {
+        match CStr::from_bytes_with_nul(
+            "Intl.DateTimeFormat().resolvedOptions().timeZone\0".as_bytes(),
+        ) {
+            Ok(s) => s,
+            Err(_) => panic!("Invalid UTF-8 data"),
+        }
+    };
+
+    unsafe {
+        NonNull::new(emscripten_run_script_string(SCRIPT.as_ptr()))
+            .ok_or_else(|| GetTimezoneError::OsError)
+            .and_then(|ptr| {
+                CStr::from_ptr(ptr.as_ptr())
+                    .to_owned()
+                    .into_string()
+                    .map_err(|_| GetTimezoneError::FailedParsingString)
+            })
+    }
 }
